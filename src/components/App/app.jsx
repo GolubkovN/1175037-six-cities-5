75516ef925d94ac6bfe93c15a43ad0abd8d96122
Golubkov Nikita <nigolubkov@gmail.com>
2020-10-03 17:56:27 +0300

    add others component, implemented routig

--- src/components/App/app.jsx
+++ src/components/App/app.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 import MainScreen from '../main-screen/main-screen';
+import Login from '../login/login';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
 
 const App = ({offersCount}) => {
-  return <MainScreen offersCount={offersCount} />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <MainScreen offersCount={offersCount} />;
+        </Route>
+        <Route exact path='/login'>
+          <Login />;
+        </Route>
+        <Route exact path='/favorites'>
+          <Favorites />;
+        </Route>
+        <Route exact path='/offer/:id'>
+          <Room />;
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 App.propTypes = {
