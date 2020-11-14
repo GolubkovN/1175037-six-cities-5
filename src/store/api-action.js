86ad8d8d2fd5c9aa1ca847eb7e11279b91e00080
Golubkov Nikita <nigolubkov@gmail.com>
2020-11-14 18:10:36 +0200

    adapt data

--- src/store/api-action.js
+++ src/store/api-action.js
@@ -1,11 +1,12 @@
 import {ActionCreator} from './action';
 import {AuthorizationStatus} from '../const';
-// import {adaptOfferToClient} from '../adapter';
+import {adaptOfferToClient} from '../adapter';
 
 const {loadOffers, requiredAuth} = ActionCreator;
 
 export const fetchOffers = () => (dispatch, _getState, api) => (
   api.get(`/hotels`)
+    .then(({data}) => data.map((item) => adaptOfferToClient(item)))
     .then(({data}) => dispatch(loadOffers(data)))
 );
 
