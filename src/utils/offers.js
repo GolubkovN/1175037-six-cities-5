6e3e69ea2d6517d8b83a669546ac4ef4a8668f09
Golubkov Nikita <nigolubkov@gmail.com>
2020-11-15 11:32:03 +0200

    delete mocks

--- src/utils/offers.js
+++ src/utils/offers.js
@@ -5,12 +5,6 @@ export const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-export const getRandomElement = (arr) => arr[getRandomInteger(0, arr.length - 1)];
-
-export const getBooleanValue = () => Boolean(getRandomInteger(0, 1));
-
-export const getRandomPhotos = () => ({url: `http://picsum.photos/248/152?r=${Math.random()}`});
-
 export const getRating = (rating) => Math.round(rating / 5 * 100) + `%`;
 
 export const getOffersByLocation = (offers) => {
